a4289eb676d17404cb1afc42ebcb7140bc51f164
nasoto <nasoto@MX08388.secgob.gob.mx>
2019-12-17 16:09:31 -0600

    Se agregan los metodos para codificar y decodificar la imagen QR

--- src/main/java/org/soto/example/CodeDecodeExample.java
+++ src/main/java/org/soto/example/CodeDecodeExample.java
@@ -4,7 +4,13 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Base64;
+
+import javax.imageio.ImageIO;
 
 import org.soto.exception.CodeDecodeQRException;
 import org.springframework.stereotype.Component;
@@ -29,9 +35,9 @@ public class CodeDecodeExample {
 		return encode;
 	}
 	
-	public String decodificarCadena(String cadena){
+	public String decodificarCadena(String cadena) throws Exception{
 		byte[] decodedBytes = Base64.getDecoder().decode(cadena.replace(" ", ""));
-		String decodedString = new String(decodedBytes);
+		String decodedString = new String(decodedBytes, "UTF-8");
 		
 		System.out.println("cadena: " +  cadena);
 		System.out.println("cadenaOrginal: " +  cadena.replace(" ", ""));
@@ -40,22 +46,84 @@ public class CodeDecodeExample {
 		return decodedString;
 	}
 	
+	/**
+	 * Este metodo genera la imagen QR
+	 * @param cadena
+	 * @return
+	 * @throws CodeDecodeQRException
+	 */
 	public BufferedImage generarImgQR(String cadena) throws CodeDecodeQRException{
 		try{
 			
 			System.out.println("cadena: " + cadena);
+			
+//			PARA MOSTRAR LA IMAGEN QR EN JASPERT REPORT
+//			Hashtable<EncodeHintType, Object> hints = new Hashtable<EncodeHintType, Object>(1);
+//	        hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+//	        BarcodeQRCode codeQR = new BarcodeQRCode(cadena, 0, 0, hints);
+//			Image imageQR = codeQR.createAwtImage(Color.BLACK, Color.WHITE);
+//			BufferedImage buffImage = new BufferedImage(imageQR.getWidth(null), imageQR.getHeight(null), BufferedImage.TYPE_BYTE_BINARY);
+//			Graphics g = buffImage.createGraphics();
+//			g.drawImage(imageQR, 0, 0, null);
+//			g.dispose();
+			
+						
+//			PARA GUARDAR LA IMAGEN QR EN UN DIRECORIO
 			BarcodeQRCode codeQR = new BarcodeQRCode(cadena, 0, 0, null);
 			Image imageQR = codeQR.createAwtImage(Color.BLACK, Color.WHITE);
-			//BufferedImage buffImage = new BufferedImage(imageQR.getWidth(null), imageQR.getHeight(null), BufferedImage.TYPE_BYTE_BINARY);
 			BufferedImage buffImage = new BufferedImage(100, 100, BufferedImage.TYPE_BYTE_BINARY);
 			Graphics g = buffImage.createGraphics();
-			//g.drawImage(imageQR, 0, 0, null);
 			g.drawImage(imageQR, 0, 0, 100, 100, null);
 			g.dispose();
+			
 			return buffImage;
 		}catch (Exception e) {
 			throw new CodeDecodeQRException("Error al generar la imagen del QR", e);
 		}
+	}
+	
+	/**
+	 * Este metodo codifica la imagen QR en una cadena en base 64
+	 * @param cadena
+	 * @return
+	 * @throws CodeDecodeQRException
+	 */
+	public String generarImgQRB64(String cadena) throws CodeDecodeQRException {
+		
+		String encodedString = null;
+		
+		try {
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			ImageIO.write(this.generarImgQR(cadena), "png", baos);
+			encodedString = new String(Base64.getEncoder().encode(baos.toByteArray()), "UTF-8");
+			//Otra forma
+			//encodedString = Base64.getEncoder().encodeToString(baos.toByteArray());
+		}catch (Exception e) {
+			throw new CodeDecodeQRException("Error al codificar la cadena de la imagen QR", e);
+		}
+		
+		return encodedString;
+		
+	}
+	
+	/**
+	 * Este metodo decodifica  la cadena en base 64 de la imagen QR 
+	 * @param imgBase64
+	 * @return
+	 * @throws IOException
+	 */
+	public BufferedImage obtenerImgQRB64(String imgBase64) throws CodeDecodeQRException {
+		try {
+			BufferedImage img = null;
+			byte[] bytes = Base64.getDecoder().decode(imgBase64);
+			InputStream codeStream = new ByteArrayInputStream(bytes);
+			img = ImageIO.read(codeStream);
+			codeStream.close();
+			return img;
+		}catch (Exception e) {
+			throw new CodeDecodeQRException("Error al decodificar la cadena de la imagen QR", e);
+		}
+		
 	}
 	
 	 
--- src/test/java/org/soto/CodeDecodeApplicationTests.java
+++ src/test/java/org/soto/CodeDecodeApplicationTests.java
@@ -33,7 +33,7 @@ public class CodeDecodeApplicationTests {
 	}
 	
 	@Test
-	public void decodificarCadenaTest(){
+	public void decodificarCadenaTest() throws Exception{
 		System.out.println("Decodificar Cadena: " +  
 				codeDecodeExample.decodificarCadena("Tm Vz dG 9y IE Fs ZW ph bm Ry by BT b3 Rv IE Ft ZX pj dW E="));
 	}
@@ -53,8 +53,40 @@ public class CodeDecodeApplicationTests {
 			}
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
+			
+		}
+	}
+	
+	
+	@Test
+	public void generarImgQRCadenaTest(){
+		try {
+			System.out.println(codeDecodeExample.generarImgQRB64("TmVzdG9yIEFsZWphbmRybyBTb3RvIEFtZXpjdWE="));
+		}catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+	
+	@Test
+	public void obtenerImgQRB64Test() {
+		String IMAGE_FORMAT = "png";
+	    String IMG_PATH = "c:/temp/qrcodeB64.png";
+	    
+	    try {
+			FileOutputStream imagenQR = new FileOutputStream(IMG_PATH);
+			try {
+				ImageIO.write(
+						codeDecodeExample.obtenerImgQRB64("iVBORw0KGgoAAAANSUhEUgAAAGQAAABkAQAAAABYmaj5AAABE0lEQVR42s3UParDMAwAYIHWQC5g8NUCuUAgq8EGrwVfIJCrCXSBQlaBnpK+V5qXps5Ybd9k/Rr0Je7wVSJA10t/A4gVsTKhqkR5I2AIXQhDc0X9LM0l+UwQ3kpZ/NSFZy6n2mqYdhWdyEJ6ndpnl15EyA58Gu/b65/E7KSXrs23msghrB0cF61InIfgQnzkshcRUxegLaUmJs8pwChYkyLi5NwwDPEgK8HPswSIWpEKuD7hUlKsiWxo1sfy6PxOjMCCOae2JhWxPL1qqUkQbUVywu31T7IA26OyFD2I1gnN1AxYk20kJ9Sy/O3nuWCdhKbfi/svsLEvRS7I2R3FC7IeUhcTjHqQ1YCzpjbnmr71d/sBi3CEs7euPYgAAAAASUVORK5CYII="), 
+							IMAGE_FORMAT, imagenQR);
+			} catch (IOException | CodeDecodeQRException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			
+		}
+	}
+	
 
 }
